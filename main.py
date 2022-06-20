45a9614d96f097bd2f6ab5d8fc6bcaf355848ca5
Deadshot96 <bhushan1810patil@gmail.com>
2022-06-20 20:25:01 +0530

    added main.py base file

---
+++ main.py
@@ -0,0 +1,13 @@
+import pygame
+import random
+from settings import *
+from colors import *
+
+class Sierpinski:
+    
+    def __init__(self):
+        pass
+    
+
+if __name__ == "__main__":
+    pass
